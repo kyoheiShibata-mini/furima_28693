5dcd36bcfe483c5e8274317f38f468cc8ae6d1bb
柴田郷平 <kani4848@gamil.com>
2020-08-21 22:00:47 +0900

    修正一回目

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-require("../calc_benefit")
 require("../sales_commission")
 
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -12,6 +12,19 @@ class Item < ApplicationRecord
 
   has_one_attached :image
 
-  validates :image, :name, :explain, :price, :category_id, :state_id, :fee_id, :place_id, :dispatch_id, :user_id, presence: true
-  validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 10000000 }
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :explain
+    validates :price, numericality: { only_integer: true, 
+                                      greater_than: 299, 
+                                      less_than: 10000000 }
+    validates :category_id
+    validates :state_id
+    validates :fee_id
+    validates :place_id
+    validates :dispatch_id
+    validates :user_id
+  end
+  
 end
