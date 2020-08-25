3b07f3423ab2b62f5ede0af68ed3118fbe41c9d4
柴田郷平 <kani4848@gamil.com>
2020-08-25 13:42:05 +0900

    商品情報編集機能実装

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -3,26 +3,4 @@ class FurimasController < ApplicationController
     @items = Item.all.order(id: "DESC")
 
   end
-
-  def new 
-    @item = Item.new
-  end
-
-  def create
-    @item =  Item.new(item_params)
-    buy_log = BuyLog.new()
-    buy_log.item_id = @item.id
-    if @item.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
-  
-end
-
-private
-
-def item_params
-  params.require(:item).permit(:image, :name, :explain, :price, :category_id, :state_id, :fee_id, :place_id, :dispatch_id).merge(user_id: current_user.id)
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,43 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :show]
+  
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(item.id)
+    else
+      render :edit
+    end
+  end
+
+  def new 
+    @item = Item.new
+  end
+
+  def create
+    @item =  Item.new(item_params)
+    buy_log = BuyLog.new()
+    buy_log.item_id = @item.id
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :explain, :price, :category_id, :state_id, :fee_id, :place_id, :dispatch_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
     @item = Item.find(params[:id])
   end
 end
